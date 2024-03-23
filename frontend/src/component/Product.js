e08b3cb1a95df50ec6e04eeacd720a6d0b1d6985
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2024-03-23 09:38:55 +0530

    admin

--- frontend/src/component/Product.js
+++ frontend/src/component/Product.js
@@ -1,47 +1,4 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import './css/product.css';
 
-// function ProductList() {
-//     const [products, setProducts] = useState([]);
-
-//     useEffect(() => {
-//         async function fetchProducts() {
-//             try {
-//                 const response = await axios.get('http://localhost:5000/api/products');
-//                 setProducts(response.data);
-//             } catch (error) {
-//                 console.error('Error fetching products:', error);
-//             }
-//         }
-//         fetchProducts();
-//     }, []);
-
-//     return (
-//         <div className="product-list-container"> 
-//             <h2>Products</h2>
-//             <ul>
-//                 {products.map(product => (
-//                     <li key={product._id} className="product-item">
-//                         <h3>{product.name}</h3>
-//                         <p>Description: {product.description}</p>
-//                         <p className="price">Price: ${product.price}</p> 
-//                         <p>Category: {product.category}</p>
-//                         <p>Quantity: {product.quantity}</p>
-//                         <p>Capacity: {product.capacity}</p>
-//                         <p>Material: {product.material}</p>
-//                         <p>Percentage: {product.percentage}</p>
-//                         <p>Country: {product.country}</p>
-//                         <img src={product.image} alt={product.name} />
-//                         <button>Add to Cart</button>
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// }
-
-// export default ProductList;
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './css/product.css';
@@ -62,6 +19,8 @@ function ProductList() {
         }, []);
     
     return (
+
+        
         <div>
             <div className="row">
                 {products.map((product, index) => (
