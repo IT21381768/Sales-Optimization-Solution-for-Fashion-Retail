d9f9011cbb3f05e8ac8185874488006397184670
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2024-04-17 19:36:33 +0530

    commit app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,11 +6,13 @@ import Login from './component/Login';
 import Register from './component/Register';
 import Home from './component/Home';
 import Profile from './component/Profile';
-import ProductList from './component/Product'; // Assuming you've renamed Product to ProductList
+import ProductList from './component/ProductList'; 
+import ProductAdd from './component/ProductAdd';
 import NavBar from './component/NavBar';
 import Cart from './component/Cart'
 import HomePraveen from './component/HomeP';
 import NotFound from './component/NotFound';
+import Admin from './component/admin';
  
 
 function App() {
@@ -27,6 +29,9 @@ function App() {
           <Route path="/product"  element={<ProductList />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/" element={<HomePraveen />} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/product/list" element={<ProductList />} />
+          <Route path="/product/add" element={<ProductAdd />} />
           <Route path="*" element={<NotFound />} />
         
         </Routes>
