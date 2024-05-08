4df86ce0064701396f2469074149d5b9d87715bc
Yenuli Minsandi Senevirathna <IT21381768@my.sliit.lk>
2024-05-08 22:51:51 +0530

    commit

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -1,42 +1,19 @@
 import React, { useState } from "react";
-
+import Lottie from 'react-lottie';
+import animationData from '../component/images/Animation - 1710827841003.json';
 import axios from "axios";
 import { Link } from "react-router-dom";
-import "./css/login.css";
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+import { toast, ToastContainer } from "react-toastify";
 
 function Login(){
     const [data, setData] = useState({ email: "", password: "" });
-	const [error, setError] = useState("");
+    const [error, setError] = useState("");
 
     const handleChange = ({ currentTarget: input }) => {
-		setData({ ...data, [input.name]: input.value });
-	}
-
-    // const handleSubmit = async (e) => {
-	// 	e.preventDefault();
-	// 	try {
-    //         if(data.email === "admin@gmail.com" && data.password === "admin"){
-    //             window.location = "/admin";
-    //         }
-    //         else{
-    //         const url = "http://localhost:5000/api/auth";
-	// 		const { data: res } = await axios.post(url, data);
-	// 		localStorage.setItem("token", res.data);
-	// 		window.location = "/profile";
-    //         }
-			
-	// 	} catch (error) {
-	// 		if (
-    //             error.response &&
-    //             error.response.status >= 400 &&
-    //             error.response.status <= 500
-    //         ) {
-    //             setError(error.response.data.message);
-    //         } else {
-    //             setError(error.response.data);
-    //         }
-	// 	}
-	// };
+        setData({ ...data, [input.name]: input.value });
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -56,49 +33,65 @@ function Login(){
         }
     };
 
-
-    return(
-<section class="login-bk">
-  <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    
-      <div class="login-tr">
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Sign in to your account
-              </h1>
-              <form class="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
-                  <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="field" placeholder="name@company.com" onChange={handleChange} value={data.email} required/>
-                  </div>
-                  <div>
-                      <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="••••••••" class="field"  onChange={handleChange} value={data.password} required/>
-                  </div>
-                  <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
-                          </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                          </div>
-                      </div>
-                      <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
-                  </div>
-                  {error && <div className="error_msg">{error}</div>}
-                  <button type="submit" class="signInBTN">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <Link to={"/Register"} class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign Up</Link>
-                  </p>
-              </form>
-          </div>
-      </div>
-  </div>
-  
-</section>
-
-    )
+    return (
+        <div>
+            <div 
+                style= {{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: '100vh',
+                    backgroundImage: `url("../images/finance-background.jpg")`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundColor:'#f9fafb',
+                }}
+            >
+                <section className="bg-gray-50 white:bg-gray-900">
+                    <div className="container" style={{ display: "flex" }}>
+                        <div style={{ width: '600px', height: '500px', marginTop: '10px' }}>
+                            <Lottie options={{ loop: true, autoplay: true, animationData: animationData }} />
+                        </div>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                                <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                                    Sign in to your account
+                                </h1>
+                                <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+                                    <div>
+                                        <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                        <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" onChange={handleChange} value={data.email} required/>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                        <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleChange} value={data.password} required/>
+                                    </div>
+                                    <div className="flex items-center justify-between">
+                                        <div className="flex items-start">
+                                            <div className="flex items-center h-5">
+                                                <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required=""/>
+                                            </div>
+                                            <div className="ml-3 text-sm">
+                                                <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                            </div>
+                                        </div>
+                                        <a href="#" className="text-sm font-medium text-white hover:underline white:text-primary-500">Forgot password?</a>
+                                    </div>
+                                    {error && <div className="error_msg">{error}</div>}
+                                    <button type="submit" className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign in</button>
+                                    <ToastContainer />
+                                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                        Don’t have an account yet? <Link to={"/Register"} className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign Up</Link>
+                                    </p>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
 }
 
 export default Login;
